f29f3776aae9cc39a9d748c78a15a670dd62757d
Ade Farquhar <adefarquhar@gmail.com>
2017-09-19 16:59:49 -0700

    put write func in read func

--- node/github-grabber/animal_fun.js
+++ node/github-grabber/animal_fun.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 let animals;
+let filtered;
+let letterAnimals;
 const firstLetter = process.argv[2];
 
 fs.readFile('./animals.txt', 'utf-8', (err,data) => {
@@ -8,11 +10,30 @@ fs.readFile('./animals.txt', 'utf-8', (err,data) => {
     return;
   }
   // console.log(data);
-  console.log(firstLetter + 'ap');
 
   animals = data.split("\n");
-  
+  filtered = animals.filter(animal => animal[0].toLowerCase() === firstLetter);
+  // console.log(filtered);
+  letterAnimals = filtered.join("\n");
+  console.log('HELLO', letterAnimals);
 });
+
+fs.writeFile(`./${firstLetter}_animals.txt`, `${letterAnimals}`, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(letterAnimals);
+    console.log(`${firstLetter}_animals created!`);
+  }
+});
+
+// fs.readFile('./animals.txt', 'utf-8', (err,data) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(data);
+// });
 
 // fs.writeFile('./example.txt', 'I will be written to example.txt.', err => {
 //   if (err) {
@@ -25,10 +46,3 @@ fs.readFile('./animals.txt', 'utf-8', (err,data) => {
 // console.log(process.argv[3]);
 
 
-// fs.readFile('./animals.txt', 'utf-8', (err,data) => {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   }
-//   console.log(data);
-// });

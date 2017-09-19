8280c250a7f014b6b83340cda92c3b8f250d4ec3
Ade Farquhar <adefarquhar@gmail.com>
2017-09-19 15:53:22 -0700

    parse animal data to array

--- node/github-grabber/animal_fun.js
+++ node/github-grabber/animal_fun.js
@@ -1,4 +1,29 @@
 const fs = require('fs');
+let animals;
+const firstLetter = process.argv[2];
+
+fs.readFile('./animals.txt', 'utf-8', (err,data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  // console.log(data);
+  console.log(firstLetter + 'ap');
+
+  animals = data.split("\n");
+  
+});
+
+// fs.writeFile('./example.txt', 'I will be written to example.txt.', err => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("file successfully written!");
+//   }
+// });
+
+// console.log(process.argv[3]);
+
 
 // fs.readFile('./animals.txt', 'utf-8', (err,data) => {
 //   if (err) {
@@ -7,11 +32,3 @@ const fs = require('fs');
 //   }
 //   console.log(data);
 // });
-
-fs.writeFile('./example.txt', 'I will be written to example.txt.', err => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("file successfully written!");
-  }
-});

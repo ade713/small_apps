0433ef1b09e7fbe5bff2e84c1a2372abd69ec97e
Ade Farquhar <adefarquhar@gmail.com>
2017-09-20 18:23:27 -0700

    remove filtered files

--- node/github-grabber/animal_fun.js
+++ node/github-grabber/animal_fun.js
@@ -7,38 +7,8 @@ let animals;
 let filtered;
 let letterAnimals;
 let animalLetter;
+let animalCache = {};
 const firstLetter = process.argv[2];
-
-fs.readFile('./animals.txt', 'utf-8', (err,data) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  // console.log(data);
-
-  letterAnimals = filterAnimals(data, firstLetter);
-  // animals = data.split("\n");
-  // filtered = animals.filter(animal => animal[0].toLowerCase() === firstLetter);
-  // letterAnimals = filtered.join("\n");
-});
-
-const filterAnimals = (animalList, letter) => {
-  animals = animalList.split("\n");
-  const filteredAnimals = animals.filter(animal => 
-    animal.startsWith(letter.toUpperCase())).join("\n");
-  return filteredAnimals;
-};
-
-setTimeout(() => {
-  fs.writeFile(`./${firstLetter}_animals.txt`, `${letterAnimals}`, err => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(letterAnimals);
-      console.log(`${firstLetter}_animals created!`);
-    }
-  });
-}, 100);
 
 // fs.readFile('./animals.txt', 'utf-8', (err,data) => {
 //   if (err) {
@@ -56,8 +26,36 @@ setTimeout(() => {
 //   }
 // });
 
-// console.log(process.argv[3]);
+// fs.readFile('./animals.txt', 'utf-8', (err,data) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   // console.log(data);
 
+//   letterAnimals = filterAnimals(data, firstLetter);
+//   // animals = data.split("\n");
+//   // filtered = animals.filter(animal => animal[0].toLowerCase() === firstLetter);
+//   // letterAnimals = filtered.join("\n");
+// });
+
+const filterAnimals = (animalList, letter) => {
+  animals = animalList.split("\n");
+  const filteredAnimals = animals.filter(animal => 
+    animal.startsWith(letter.toUpperCase())).join("\n");
+  return filteredAnimals;
+};
+
+// setTimeout(() => {
+//   fs.writeFile(`./${firstLetter}_animals.txt`, `${letterAnimals}`, err => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(letterAnimals);
+//       console.log(`${firstLetter}_animals created!`);
+//     }
+//   });
+// }, 100);
 
 
 // const server = http.createServer((req, res) => {
@@ -68,24 +66,47 @@ setTimeout(() => {
 // server.listen(8000, () => console.log("I'm listening on Port 8000! ^__^"));
 
 
-// const server = http.createServer((req, res) => {
-//   const query = req.url.split('?')[1];
-  
-//   if (query === 'undefined') {
-//     fs.readFile('./animals.txt', 'utf-8', (err,data) => {
-//       if (err) {
-//         console.log(err);
-//         return;
-//       }
-//       // console.log(data);
-//     });
-//   }
+const server = http.createServer((req, res) => {
+  const query = req.url.split('?')[1];
 
-//   animalLetter = querystring.parse(query).letter;
+  if (query === undefined) {
+    fs.readFile('./animals.txt', 'utf-8', (err,data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
 
-//   console.log(animalLetter);
-//   res.write(`${animalLetter}`);
-//   res.end();
-// });
+      if (animalCache['all'] === undefined) {
+        animalCache['all'] = data;
+      }
+      res.end(animalCache['all']);
+    });
+  } else {
+    animalLetter = querystring.parse(query).letter;
 
-// server.listen(8000, () => console.log('Listening on port 8000!'));
+    fs.readFile('./animals.txt', 'utf-8', (err,data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      if (animalCache[animalLetter]) {
+        res.write(`Animals that start with the letter ${animalLetter}:`);
+        res.write(`${animalCache[animalLetter]}`);
+        res.end();
+      } else {
+        letterAnimals = filterAnimals(data, animalLetter);
+        animalCache[animalLetter] = letterAnimals;
+
+        res.write(`${animalCache[animalLetter]}`);
+        res.end();
+      }
+    });
+  }
+
+  // console.log(animalLetter);
+  // res.write(`${animalLetter}`);
+  // res.end();
+});
+
+server.listen(8000, () => console.log('Listening on port 8000'));

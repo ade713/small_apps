07406b9bc80eb1b20ed2d42a6348da3c49121a40
Ade Farquhar <adefarquhar@gmail.com>
2017-09-25 15:15:57 -0700

    change dir path

--- node/chat-app-skeleton/app.js
+++ node/chat-app-skeleton/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // app.get('/', (req, res) => {
 //   res.send('h3ll0 w0rld!!!');
@@ -7,6 +8,9 @@ const app = express();
 
 
 app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
 app.listen(3003, () => {
   console.log('Chat App listening on port 3003');

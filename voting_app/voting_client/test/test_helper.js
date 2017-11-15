16fc4be2622670ce0423b9012e2367958e4636d9
Ade Farquhar <adefarquhar@gmail.com>
2017-11-14 21:14:46 -0800

    add test scripts

--- voting_app/voting_client/test/test_helper.js
+++ voting_app/voting_client/test/test_helper.js
@@ -1,4 +1,6 @@
 import jsdom from 'jsdom';
+import chai from 'chai';
+import chaiImmutable from 'chai-immutable';
 
 const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
 const win = jsdom.defaultView;
@@ -6,3 +8,10 @@ const win = jsdom.defaultView;
 global.document = doc;
 global.window = win;
 
+Object.keys(window).forEach((key) => {
+  if (!(key in global)) {
+    global[key] = window[key];
+  }
+});
+
+chai.use(chaiImmutable);

aef25d73752cf49a388c833685fec9ef283af680
Ade Farquhar <adefarquhar@gmail.com>
2017-11-19 23:58:04 -0800

    setup react in index

--- voting_app/voting_client/src/index.jsx
+++ voting_app/voting_client/src/index.jsx
@@ -1,2 +1,12 @@
-console.log('HEELLLOOOO....... :D');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import Voting from './components/Voting';
+
+
+const pair = ['Trainspotting', '28 Days Later'];
+
+ReactDOM.render(
+  <Voting pair={ pair } />,
+  document.getElementById('app')
+);

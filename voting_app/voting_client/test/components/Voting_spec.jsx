d32c786f9eefc4e1d8e83fcc23bdcf39d327df66
Ade Farquhar <adefarquhar@gmail.com>
2017-11-23 23:09:17 -0800

    add simulate click button test

--- voting_app/voting_client/test/components/Voting_spec.jsx
+++ voting_app/voting_client/test/components/Voting_spec.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { renderIntoDocument,
-         scryRenderedDOMComponentsWithTag } from 'react-dom/test-utils';
+         scryRenderedDOMComponentsWithTag,
+         Simulate } from 'react-dom/test-utils';
 // alternative: import ReactTestUtils from 'react-dom/test-utils';
 import { expect } from 'chai';
 
@@ -18,6 +19,20 @@ describe('Voting', () => {
     expect(buttons.length).to.equal(2);
     expect(buttons[0].textContent).to.equal("Trainspotting");
     expect(buttons[1].textContent).to.equal("28 Days Later");
+  });
+
+  it('invokes callback when a button is clicked', () => {
+    let votedWidth;
+    const vote = (entry) => votedWidth = entry;
+
+    const component = renderIntoDocument(
+      <Voting pair={ ["Trainspotting", "28 Days Later"] }
+              vote={ vote } />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    Simulate.click(buttons[0]);
+
+    expect(votedWidth).to.equal("Trainspotting");
   });
 
 });

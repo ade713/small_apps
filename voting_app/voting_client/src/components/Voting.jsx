0acf4e9314d7464c122eee63d9167ddc15f4638d
Ade Farquhar <adefarquhar@gmail.com>
2017-11-24 00:25:54 -0800

    add winner component

--- voting_app/voting_client/src/components/Voting.jsx
+++ voting_app/voting_client/src/components/Voting.jsx
@@ -1,26 +1,5 @@
 import React from 'react';
 
-// export default React.createClass({
-//   getPair: function(){
-//     return this.props.pair || [];
-//   },
-//   isDisabled: function() {
-//     return !!this.props.hasVoted;
-//   },
-//   render: function(){
-//     return (
-//       <div className="voting">
-//         { this.getPaiir().map(entry => 
-//           <button key={ entry }
-//                   onClick={ () => this.props.vote(entry) }>
-//             <h1>{ entry }</h1>
-//           </button>
-//         )}
-//       </div>
-//     );
-//   }
-// });
-
 
 export default class Voting extends React.Component {
   constructor(props){
@@ -35,14 +14,22 @@ export default class Voting extends React.Component {
     return !!this.props.hasVoted;
   }
 
+  hasVotedFor(entry) {
+    return this.props.hasVoted === entry;
+  }
+
   render() {
     return(
       <div className="voting">
-        { this.getPair().map(entry => 
-          <button key={ entry }
-                  onClick={ () => this.props.vote(entry) }>
-            <h1>{ entry }</h1>
-          </button>
+        { this.props.winner ? 
+          <div ref="winner">Winner is { this.props.winner }!</div> : 
+          this.getPair().map(entry => 
+            <button key={ entry }
+                    onClick={ () => this.props.vote(entry) }>
+              <h1>{ entry }</h1>
+              { this.hasVotedFor(entry) ? 
+                  <div className="label">Voted</div> : null }
+            </button>
         )}
       </div>
     );

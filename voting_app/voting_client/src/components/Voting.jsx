940f97667603c06423a68d712e710bff795d5a06
Ade Farquhar <adefarquhar@gmail.com>
2017-11-24 00:00:34 -0800

    fix voting class

--- voting_app/voting_client/src/components/Voting.jsx
+++ voting_app/voting_client/src/components/Voting.jsx
@@ -1,18 +1,50 @@
 import React from 'react';
 
-export default React.createClass({
-  getPaiir: function(){
+// export default React.createClass({
+//   getPair: function(){
+//     return this.props.pair || [];
+//   },
+//   isDisabled: function() {
+//     return !!this.props.hasVoted;
+//   },
+//   render: function(){
+//     return (
+//       <div className="voting">
+//         { this.getPaiir().map(entry => 
+//           <button key={ entry }
+//                   onClick={ () => this.props.vote(entry) }>
+//             <h1>{ entry }</h1>
+//           </button>
+//         )}
+//       </div>
+//     );
+//   }
+// });
+
+
+export default class Voting extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  getPair() {
     return this.props.pair || [];
-  },
-  render: function(){
-    return (
+  }
+
+  isDisabled() {
+    return !!this.props.hasVoted;
+  }
+
+  render() {
+    return(
       <div className="voting">
-        { this.getPaiir().map(entry => 
-          <button key={ entry }>
+        { this.getPair().map(entry => 
+          <button key={ entry }
+                  onClick={ () => this.props.vote(entry) }>
             <h1>{ entry }</h1>
           </button>
         )}
       </div>
     );
   }
-});
+}

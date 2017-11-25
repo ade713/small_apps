51b396682ac2f60cb2f26256983e1bf4cdcbeb63
Ade Farquhar <adefarquhar@gmail.com>
2017-11-25 01:02:55 -0800

    add test rendering pure component

--- voting_app/voting_client/test/components/Voting_spec.jsx
+++ voting_app/voting_client/test/components/Voting_spec.jsx
@@ -57,4 +57,36 @@ describe('Voting', () => {
     expect(buttons[0].textContent).to.contain('Voted');
   });
 
+  it('renders just the winner when there is one', () => {
+    const component = renderIntoDocument(
+      <Voting winner="Trainspotting" />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    expect(buttons.length).to.equal(0);
+
+    const winner = ReactDOM.findDOMNode(component.refs.winner);
+    expect(winner).to.be.ok;
+    expect(winner.textContent).to.contain('Trainspotting');
+  });
+
+  it('renders as a pure component', () => {
+    const pair = ['Trainspotting', '28 Days Later'];
+    const container = document.createElement('div');
+    let component = ReactDOM.render(
+      <Voting pair={ pair } />,
+      container
+    );
+
+    let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('Trainspotting');
+
+    pair[0] = 'Sunshine';
+    component = ReactDOM.render(
+      <Voting pair={ pair } />,
+      container
+    );
+    firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('Trainspotting');
+  });
+
 });

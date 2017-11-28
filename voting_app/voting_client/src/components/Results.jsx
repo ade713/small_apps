3bf85454d9ecf1a70716cc1ba7edbc4e1df8807e
Ade Farquhar <adefarquhar@gmail.com>
2017-11-28 02:30:27 -0800

    add logic to results

--- voting_app/voting_client/src/components/Results.jsx
+++ voting_app/voting_client/src/components/Results.jsx
@@ -5,10 +5,18 @@ export default class Results extends React.PureComponent {
     super(props);
   }
 
+  getPair() {
+    return this.props.pair;
+  }
+
   render() {
     return (
-      <div>
-        Hello from results!!! o__O
+      <div className="results">
+        { this.getPair().map(entry =>
+          <div key={ entry } className="entry">
+            <h1>{ entry }</h1>
+          </div>
+        )}
       </div>
     );
   }
--- voting_app/voting_client/src/index.jsx
+++ voting_app/voting_client/src/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route,
-         Router } from 'react-router';
+import { Route } from 'react-router';
 import { HashRouter } from 'react-router-dom';
 
 import App from './components/App';
-import Voting from './components/Voting';
-import Results from './components/Results';
+// import Voting from './components/Voting';
+// import Results from './components/Results';
 
 
 const pair = ['Trainspotting', '28 Days Later'];

55aaf56166d5ad0f4e12550e84b08bebf0e90590
Ade Farquhar <adefarquhar@gmail.com>
2017-11-30 16:47:01 -0800

    remove dead code

--- voting_app/voting_client/src/index.jsx
+++ voting_app/voting_client/src/index.jsx
@@ -4,20 +4,9 @@ import { Route } from 'react-router';
 import { HashRouter } from 'react-router-dom';
 
 import App from './components/App';
-// import Voting from './components/Voting';
-// import Results from './components/Results';
 
 
 const pair = ['Trainspotting', '28 Days Later'];
-
-// const routes = (
-//   <div>
-//     <Route component={ App }> */}
-//       <Route path="/results" component={ Results } />
-//       <Route exact path="/" component={ Voting } />
-//     </Route>
-//   </div>
-// );
 
 ReactDOM.render(
   <HashRouter>

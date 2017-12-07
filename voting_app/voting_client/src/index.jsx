e094a59893d4126b40b3d28d08bb9c957ed9fbba
Ade Farquhar <adefarquhar@gmail.com>
2017-12-07 00:13:03 -0800

    include middleware index

--- voting_app/voting_client/src/index.jsx
+++ voting_app/voting_client/src/index.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route } from 'react-router';
 import { HashRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore,
+         applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import io from 'socket.io-client';
 
 import App from './components/App';
 import reducer from './reducer';
 import { setState } from './action_creators';
+import remoteActionMiddleware from './remote_action_middleware';
 
-
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware
+)(createStore);
 const store = createStore(reducer);
-const socket = io(`${ location.protocol }//${ location.hostname }:8090`);
 
+const socket = io(`${ location.protocol }//${ location.hostname }:8090`);
 socket.on('state', state => 
   store.dispatch(setState(state))
 );

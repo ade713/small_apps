6831984bd944dc4594057b928c7d92e68d42b39b
Ade Farquhar <adefarquhar@gmail.com>
2018-01-05 00:49:43 -0800

    hanlde range/volume functioning

--- js30/JavaScript30-master/11 - Custom Video Player/scripts.js
+++ js30/JavaScript30-master/11 - Custom Video Player/scripts.js
@@ -8,6 +8,40 @@ const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 // build out functions
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
 
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+  console.log('update button');
+}
+
+function skip() {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+  // console.log(this.name);
+  // console.log(this.value);
+}
+
+function handleProgress() {
+  
+}
 
 // connect event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button =>  button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
